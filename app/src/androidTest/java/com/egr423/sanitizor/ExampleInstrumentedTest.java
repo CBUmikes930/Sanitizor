04b51de7e1fdeb7ebe170df5f851832d17d9271f
mikes930 <Micahwilliam.steinbock@calbaptist.edu>
2020-10-07 13:16:44 -0700

    Updated graphics and player speed

--- app/src/androidTest/java/com/egr423/sanitizor/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/egr423/sanitizor/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.egr423.sanitizor", appContext.getPackageName());
+        assertEquals("com.egr423.player", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/egr423/sanitizor/Player.java
+++ app/src/main/java/com/egr423/sanitizor/Player.java
@@ -1,8 +1,13 @@
 package com.egr423.sanitizor;
 
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+import androidx.core.content.res.ResourcesCompat;
 
 /**
  * Created by Micah Steinbock on 10/6/2020
@@ -11,19 +16,37 @@ import android.graphics.PointF;
  */
 public class Player {
 
-    //Temp Variable to make player a circle until graphics are made
-    public final int RADIUS = 100;
-    public final int BOTTOM_PADDING = 50;
+    //Player position values
+    private final int WIDTH = 150; //Change WIDTH to change overall image size
+    private final int HEIGHT; //Calculated when image is loaded in order to preserve aspect ratio
+    private final int BOTTOM_PADDING = 50; //Padding to keep image off of bottom
 
+    //Speed multiplier for the movement speed based on accelerometer
+    private final double SPEED = 1.5;
+
+    //Used when drawing the default player (Circle if image can't be loaded)
     private Paint mPaint;
-    //The location of the center of the player
-    private PointF mCenter;
 
+    //The location of the top left corner of the player
+    private PointF mTopLeft;
+
+    //The image resource for the player
+    private Drawable mPlayerImage;
     //Screen Dimensions
     private int mSurfaceWidth;
     private int mSurfaceHeight;
 
-    public Player(int surfaceWidth, int surfaceHeight) {
+    public Player(int surfaceWidth, int surfaceHeight, Context context) {
+        //Load the image from the resources
+        mPlayerImage = ResourcesCompat.getDrawable(context.getResources(), R.drawable.player, null);
+        if (mPlayerImage != null) {
+            //If image was loaded, then calculate it's relative height compared to the WIDTH (aspect ratio)
+            HEIGHT = (int) (WIDTH * ((float) mPlayerImage.getIntrinsicHeight() / (float) mPlayerImage.getIntrinsicWidth()));
+        } else {
+            //Couldn't load, so post a message and set HEIGHT = WIDTH
+            Log.d("Player Error", "Could not load mPlayerImage from resource: R.drawable.player");
+            HEIGHT = WIDTH;
+        }
         //Set Screen dimensions
         mSurfaceWidth = surfaceWidth;
         mSurfaceHeight = surfaceHeight;
@@ -36,30 +59,38 @@ public class Player {
         mPaint.setColor(0xffaaaaff);
     }
 
-    public void setCenter(int x, int y) {
-        mCenter.x = x;
-        mCenter.y = y;
+    public void setPosition(int x, int y) {
+        mTopLeft.x = x;
+        mTopLeft.y = y;
     }
 
     public void move(PointF velocity) {
         //Move center by velocity on x-axis, but anchor y
-        mCenter.offset(-velocity.x, 0);
+        mTopLeft.offset((float) (-velocity.x * SPEED), 0);
 
         //Check if still on screen
-        if (mCenter.x > mSurfaceWidth - RADIUS) {
+        if (mTopLeft.x + WIDTH > mSurfaceWidth) {
             //If too far right, then set it's right side to the edge of screen
-            mCenter.x = mSurfaceWidth - RADIUS;
-        } else if (mCenter.x < RADIUS) {
+            mTopLeft.x = mSurfaceWidth - WIDTH;
+        } else if (mTopLeft.x < 0) {
             //If too far left, then set it's left side to the edge of the screen
-            mCenter.x = RADIUS;
+            mTopLeft.x = 0;
         }
     }
 
     public void draw(Canvas canvas) {
-        canvas.drawCircle(mCenter.x, mCenter.y, RADIUS, mPaint);
+        if (mPlayerImage != null) {
+            //If we have a player image, then draw it
+            mPlayerImage.setBounds((int) mTopLeft.x, (int) mTopLeft.y, (int) mTopLeft.x + WIDTH, (int) mTopLeft.y + HEIGHT);
+            mPlayerImage.draw(canvas);
+        } else {
+            //If we don't have a player image, then draw a circle
+            canvas.drawCircle(mTopLeft.x + (float) (WIDTH / 2), mTopLeft.y + (float) (WIDTH / 2), (float) WIDTH / 2, mPaint);
+        }
     }
 
     public void setStartPosition() {
-        mCenter = new PointF( (float) mSurfaceWidth / 2, mSurfaceHeight - (BOTTOM_PADDING + RADIUS));
+        //Set player image to the center of the screen and the bottom off the screen's bottom by the padding
+        mTopLeft = new PointF( (float) (mSurfaceWidth - WIDTH) / 2, mSurfaceHeight - (BOTTOM_PADDING + HEIGHT));
     }
 }
