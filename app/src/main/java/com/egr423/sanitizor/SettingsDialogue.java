621b61c17ffb55b4c6b84e7e63eea697e7df59af
Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>
2020-10-07 18:25:48 -0700

    added SettingsDialogueListener in SettingsDialogue.java

--- app/src/main/java/com/egr423/sanitizor/SettingsDialogue.java
+++ app/src/main/java/com/egr423/sanitizor/SettingsDialogue.java
@@ -1,5 +1,6 @@
 package com.egr423.sanitizor;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,8 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class SettingsDialogue extends BottomSheetDialogFragment {
 
+    private SettingsDialogListener dialogListener;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -19,7 +22,22 @@ public class SettingsDialogue extends BottomSheetDialogFragment {
 
 
 
-
         return v;
+    }
+
+    public interface SettingsDialogListener {
+        void onButtonClicked();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        try {
+            dialogListener = (SettingsDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() +
+                    " must implement SettingsDialogListener");
+        }
     }
 }
